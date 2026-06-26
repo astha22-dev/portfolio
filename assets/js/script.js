e0b4cd05793cf2dbccd744d6c21b2429ea9f59c8
astha22-dev <ashtha.kittu@gmail.com>
2026-06-26 14:05:26 +0530

    Implement loader hide on window load
    Added a loader hide functionality on window load.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
+// Hide Loader
+window.addEventListener("load", () => {
 
+    const loader = document.getElementById("loader");
+
+    loader.style.opacity = "0";
+
+    loader.style.visibility = "hidden";
+
+    setTimeout(() => {
+
+        loader.remove();
+
+    }, 500);
+
+});
